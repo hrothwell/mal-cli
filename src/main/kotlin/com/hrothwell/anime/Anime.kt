ee1149d81738c70489d0da815bc182a0e5fb64bf
hrothwell <hrothwell@css.edu>
2022-10-02 21:20:08 -0700

    Revert "Merge pull request #11 from hrothwell/move-command"
    This reverts commit 7424b16f52f1aaf72d8fefb5278ec23660e5c6ec, reversing
    changes made to 512ec95c88f433520e932a6c2cad79d157f00e9e.

--- src/main/kotlin/com/hrothwell/anime/Anime.kt
+++ src/main/kotlin/com/hrothwell/anime/Anime.kt
@@ -1,22 +1,33 @@
-package com.hrothwell.anime.commands
+package com.hrothwell.anime
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.choice
 import com.github.kittinunf.fuel.Fuel
-import com.hrothwell.anime.domain.*
+import com.github.kittinunf.fuel.core.Response
+import com.hrothwell.anime.domain.ListStatus
+import com.hrothwell.anime.domain.MALUserListResponse
+import com.hrothwell.anime.domain.UserSecrets
 import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import java.io.File
+import kotlin.system.exitProcess
 
-class Random: CliktCommand(
+class Anime : CliktCommand(
   help = "pick a random anime from your MAL lists",
   epilog = """
     extended user guide, source code, etc - https://github.com/hrothwell/anime-cli
   """.trimIndent()
 ) {
+  val home = System.getProperty("user.home")
+  val secretLocation = "$home/anime-cli/mal-secret.json"
+  val jsonReader = Json{ignoreUnknownKeys = true}
+
   private val possibleListStatusValues = ListStatus.values().map { it.malValue }.toTypedArray()
+  private val RED = "\u001b[31m"
   private val user by option("-u", "--user-name", help = """
-    user name. if not provided this will default to "user_name" value in "$secretLocation"
+    user name. if not provided this will default to "user_name" value in "${home}/anime-cli/mal-secret.json"
   """.trimIndent())
 
   private val list by option(
@@ -30,9 +41,10 @@ class Random: CliktCommand(
     getRandomAnime(user, list)
   }
 
-  private fun getRandomAnime(user: String?, list: String) {
+  fun getRandomAnime(user: String?, list: String) {
 
     val clientSecrets = getUserSecrets()
+
     val headers = "X-MAL-CLIENT-ID" to clientSecrets.client_id
     val listStatus = "status" to list
     val limit = "limit" to 1000
@@ -58,8 +70,39 @@ class Random: CliktCommand(
     }
   }
 
-  // can't put in constants as echo is protected
-  fun echoError(msg: String) {
+  fun handlePotentialHttpErrors(response: Response) {
+    if(response.statusCode != 200){
+      echoError("${response.statusCode} error returned from MAL: ${response.responseMessage}")
+      quickErrorHelp()
+      exitProcess(1)
+    }
+  }
+
+  fun getUserSecrets(): UserSecrets{
+    return try {
+      val clientSecretsJsonContent = File(secretLocation).readText()
+      jsonReader.decodeFromString<UserSecrets>(clientSecretsJsonContent)
+    } catch (t: Throwable) {
+      echoError("""
+        You need to place your MAL client id in this file: $secretLocation. File contents should look like:
+        {
+          "user_name": "my user name",
+          "client_id": "my MAL API client ID"
+        }
+      """.trimIndent())
+      exitProcess(1)
+    }
+  }
+
+  fun quickErrorHelp(){
+    echoError("""
+      Quick help / common situations:
+        - 400/403: no client_id set in mal-secret.json or said client_id is invalid. See https://myanimelist.net/apiconfig
+        - 404: user was not found. Was the user you passed in ($user) a real user? If no user passed in, is your mal-secret.json setup correctly?
+    """.trimIndent())
+  }
+
+  private fun echoError(msg: String) {
     echo("$RED $msg", err = true)
   }
 }
--- src/main/kotlin/com/hrothwell/anime/Main.kt
+++ src/main/kotlin/com/hrothwell/anime/Main.kt
@@ -1,14 +1,8 @@
 package com.hrothwell.anime
 
-import com.github.ajalt.clikt.core.subcommands
-import com.hrothwell.anime.commands.Anime
-import com.hrothwell.anime.commands.Random
-
 object Main {
   @JvmStatic
   fun main(args: Array<String>) {
-    Anime()
-      .subcommands(Random())
-      .main(args)
+    Anime().main(args)
   }
 }

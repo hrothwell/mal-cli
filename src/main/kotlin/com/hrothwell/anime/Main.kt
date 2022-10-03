f8d63f1949503d8a787b97d7d5550e34c099acfb
hrothwell <hrothwell@css.edu>
2022-10-02 20:47:35 -0700

    starting login action

--- src/main/kotlin/com/hrothwell/anime/Main.kt
+++ src/main/kotlin/com/hrothwell/anime/Main.kt
@@ -2,13 +2,14 @@ package com.hrothwell.anime
 
 import com.github.ajalt.clikt.core.subcommands
 import com.hrothwell.anime.commands.Anime
+import com.hrothwell.anime.commands.Login
 import com.hrothwell.anime.commands.Random
 
 object Main {
   @JvmStatic
   fun main(args: Array<String>) {
     Anime()
-      .subcommands(Random())
+      .subcommands(Random(), Login())
       .main(args)
   }
 }
---
+++ src/main/kotlin/com/hrothwell/anime/commands/Login.kt
@@ -0,0 +1,83 @@
+package com.hrothwell.anime.commands
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.kittinunf.fuel.Fuel
+import com.hrothwell.anime.domain.getUserSecrets
+import com.sun.net.httpserver.HttpExchange
+import com.sun.net.httpserver.HttpHandler
+import com.sun.net.httpserver.HttpServer
+import java.lang.Thread.sleep
+import java.net.InetSocketAddress
+import kotlin.random.Random
+
+
+// TODO requires the whole oauth setup bullshit
+class Login: CliktCommand(
+  help = """
+    Login/authorize this app to have more access to your MAL account
+  """.trimIndent()
+) {
+
+  override fun run() {
+    login()
+  }
+
+  /**
+   * TODO OAuth Steps
+   * - Generate a code verifier and challenge (one time only?)
+   * - request oauth authentication (one time only?)
+   *  - MAL authenticates user, the user authorizes the client
+   * - MAL redirects client to the REDIRECT URI we provide...
+   *    - TODO this is what worries me, we have a CLI app so idk how this will quite work...
+   * - Store this authorization code and this is what is used for refresh and access tokens?
+   * -
+   */
+
+  private fun login(){
+    val userSecrets = getUserSecrets()
+    val charPool = ('a'..'z') + ('A'..'Z') + ('0'..'9') + listOf('-', '.', '_', '~')
+    val randomString = (1..100)
+      .map{ Random.nextInt(0, charPool.size)}
+      .map(charPool::get)
+      .joinToString("")
+
+    val myServerEndpoint = "http://localhost:8080/anime"
+
+    // TODO MAL specific code. If they already are "signed in" and we have an oauth token that is valid, probably don't need to do this
+//    val responseType = "response_type" to "code"
+//    val clientId = "client_id" to userSecrets.client_id
+//    val codeChallenge = "code_challenge" to randomString
+//    val redirectUri = "redirect_uri" to myServerEndpoint
+//    val params = listOf(responseType, clientId, codeChallenge, redirectUri)
+//    val authorizationResult = Fuel.get("https://myanimelist.net/v1/oauth2/authorize", parameters = params)
+
+    // TODO start a local server, then use that as redirect url?
+    //  need HttpHandler and HttpContext
+    val localServer = HttpServer.create()
+    val httpContext = localServer.createContext("/anime", MyHttpHandler())
+    localServer.bind(InetSocketAddress(8080), 0)
+    localServer.start()
+
+    // TODO put auth call here
+    val result = Fuel.get("$myServerEndpoint?hello=world").response().third.get()
+    val resultString = String(result)
+    println("resultString $resultString")
+
+    localServer.stop(0)
+  }
+
+  // TODO Probably need to also return some html that just tells user to go back to CLI
+  private class MyHttpHandler: HttpHandler{
+    override fun handle(exchange: HttpExchange?) {
+      println("handling http request")
+      val f = exchange?.requestURI
+      // TODO this will store values we need for further calls, where to store them?
+      val params = f?.query
+      println("Here is query string: $params")
+      exchange?.sendResponseHeaders(200, 0)
+      val outStream = exchange?.responseBody
+      outStream?.write(params?.toByteArray()!!)
+      exchange?.close()
+    }
+  }
+}

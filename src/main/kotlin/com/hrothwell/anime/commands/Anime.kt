41da924308ebd2c4e5d8c72b5d092f2839824809
hrothwell <hrothwell@css.edu>
2022-10-05 22:08:21 -0700

    update readme

--- src/main/kotlin/com/hrothwell/anime/commands/Anime.kt
+++ src/main/kotlin/com/hrothwell/anime/commands/Anime.kt
@@ -5,7 +5,7 @@ import com.github.ajalt.clikt.core.CliktCommand
 class Anime : CliktCommand(
   help = "CLI for interacting with MAL",
   epilog = """
-    extended user guide, source code, etc - https://github.com/hrothwell/anime-cli
+    extended user guide, source code, report issues, etc - https://github.com/hrothwell/anime-cli
   """.trimIndent()
 ) {
   override fun run() = Unit

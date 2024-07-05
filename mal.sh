601f552dee37035a0491840652fa662acab0a187
HaidenRothwell <46227616+hrothwell@users.noreply.github.com>
2024-07-05 12:27:53 -0700

    set version in helper

--- mal.sh
+++ mal.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-docker run --mount type=bind,source="$PATH_TO_MAL_CLI",target="/root/mal-cli" -p "8080:8080" "hrothwell/mal-cli:latest" $@
+VERSION="v1.0.0"
+docker run --mount type=bind,source="$PATH_TO_MAL_CLI",target="/root/mal-cli" -p "8080:8080" "hrothwell/mal-cli:$VERSION" $@
